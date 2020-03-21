5cb52302d4f8f77c579584e370dd741777717030
unknown <you@example.com>
2020-03-21 06:43:36 +0600

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import Calendar from './Components/Calendar';
+
+import Calendar from './Components/Calendar/';
+
 
 const style = {
-  position: 'relative',
-  margin: '50px auto'
+  position: "relative",
+  margin: "50px auto",
+  
 }
-function App() {
-  return (
-    <div className="App">
-      <Calendar style={style} width="302px"/>
-    </div>
-  );
+
+class App extends Component {
+  onDayClick = (e, day) => {
+    alert(day);
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <Calendar style={style}  
+          onDayClick={(e, day)=> this.onDayClick(e, day)}/>     
+      </div>
+    );
+  }
 }
 
 export default App;
